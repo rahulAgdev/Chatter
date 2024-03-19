a5e7fa08c81763518904c539b736a8a068889ed7
Rahul Agarwal <rahulagdev@gmail.com>
2024-03-19 10:28:02 +0530

    Adding Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Button from "./components/Button";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
 
-export default App
+export default App;
