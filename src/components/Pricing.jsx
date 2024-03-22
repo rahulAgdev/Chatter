5700a40e3e47cde5eb2b214d0e43cc31a994ee54
Rahul Agarwal <rahulagdev@gmail.com>
2024-03-22 16:09:48 +0530

    Trying to resolve deployement issue

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,4 +1,4 @@
-import Section from "./Section";
+import Section from "../components/Section";
 import { smallSphere, stars } from "../assets";
 import { MouseParallax } from "react-just-parallax";
 import Heading from "./Heading";
