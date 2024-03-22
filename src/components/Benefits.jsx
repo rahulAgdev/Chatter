0bef9d3ce7f3d986d4f59176fe4da6edc31cfb3f
Rahul Agarwal <rahulagdev@gmail.com>
2024-03-22 15:46:01 +0530

    Trying to resolve deployement issue

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,7 +1,7 @@
 import { benefits } from "../constants";
 import Heading from "./Heading";
-import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
+import Section from "./Section";
 import { GradientLight } from "./design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
 
